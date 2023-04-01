eaef682ac47db9e310969684ddade1430707a41f
t0ur1st <tanyaodan@qq.com>
2023-04-01 13:37:53 +0800

    update

--- src/get_info.py
+++ src/get_info.py
@@ -13,9 +13,9 @@ def project_info(pid, save=False):
     if save:
         log_dir = os.path.join(defects4j_dir, f'logs/{pid}')
         os.makedirs(log_dir, exist_ok=True)
-        log_file = os.path.join(log_dir, f'{pid}-Info.log')
-        with open(log_file, 'w') as f:
-            process = subprocess.Popen(query, shell=True, executable='/bin/bash', cwd=defects4j_dir, stdout=f, stderr=subprocess.STDOUT)
+        log_file_path = os.path.join(log_dir, f'{pid}-Info.log')
+        with open(log_file_path, 'w') as log_file:
+            process = subprocess.Popen(query, shell=True, executable='/bin/bash', cwd=defects4j_dir, stdout=log_file, stderr=subprocess.STDOUT)
             process.wait()
     else:
         process = subprocess.Popen(query, shell=True, executable='/bin/bash', cwd=defects4j_dir, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -39,6 +39,28 @@ def show_all_projects_info():
             print(f'{res[0]}, {res[1]}')
     
 
+def bug_info(pid, vid):
+    query = f'defects4j info -p {pid} -b {vid}'
+    log_dir = os.path.join(defects4j_dir, f'logs/{pid}')
+    os.makedirs(log_dir, exist_ok=True)
+    log_file_path = os.path.join(log_dir, f'{vid}_bug-info.log')
+    with open(log_file_path, 'w') as log_file:
+        process = subprocess.Popen(query, shell=True, executable='/bin/bash', cwd=defects4j_dir, stdout=log_file, stderr=subprocess.STDOUT)
+        process.wait()
+
+
+def log_all_bugs_info():
+    """ Log information about all bugs """
+    bugs_number = {'Chart': 26, 'Cli': 40, 'Closure': 176, 'Codec': 18, 'Collections': 28, 'Compress': 47, 'Csv': 16, 
+                  'Gson': 18, 'JacksonCore': 26, 'JacksonDatabind': 112, 'JacksonXml': 6, 'Jsoup': 93, 'JxPath': 22, 
+                  'Lang': 65, 'Math': 106, 'Mockito': 38, 'Time': 27}
+    with ThreadPoolExecutor() as executor:
+        for pid in project_ids:
+            vid_cnt = bugs_number.get(pid, 0)
+            executor.map(bug_info, [pid]*vid_cnt, range(1, vid_cnt+1))
+
+
 if __name__ == '__main__':
     # show_all_projects_info()
-    log_all_projects_info()
+    # log_all_projects_info()
+    log_all_bugs_info()
